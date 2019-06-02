4f9fcc161c7eb08c2b9339e6ffcf3b39a932353f
sajad1993gh <sajad1993gh@gmail.com>
2019-06-02 19:07:45 -0400

    more

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var wins = 0;
+var losses = 0;
+var random = 0;
+var totalScore = 0;
+
+
+var crystals = [
+    $("#red"), $("#drop"), $("#yellow"), $("#blue")
+]
+
+$(".col-3").on("click", function() {
+    // console.log("hello");
+});
