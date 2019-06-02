38e278f76a894aa9b2a1ce59831e54565c17ba42
sajad1993gh <sajad1993gh@gmail.com>
2019-06-02 19:18:02 -0400

    more

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 var wins = 0;
 var losses = 0;
-var random = 0;
+var crystalRandom = 0;
+var computerRandom = 0;
 var totalScore = 0;
 
 
@@ -8,6 +9,26 @@ var crystals = [
     $("#red"), $("#drop"), $("#yellow"), $("#blue")
 ]
 
+function start() {
+    computerRandom = Math.floor(Math.random() * 102) + 19;
+    $("#random").text(computerRandom);
+}
+
+start();
+
 $(".col-3").on("click", function() {
     // console.log("hello");
+    crystalRandom = Math.floor(Math.random() * 12) + 1;
+    $(this).text(crystalRandom);
+    totalScore += crystalRandom;
+
+    $("#totalScore").text(totalScore);
+
+    if (totalScore > computerRandom) {
+        ++losses;
+        $("losses").text(losses);
+    } else if (totalScore === computerRandom) {
+        ++wins;
+        $("wins").text(wins);
+    }
 });
