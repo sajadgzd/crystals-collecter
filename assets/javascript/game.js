6ff718173aaf25a3ebb4e51f2d83ae8b676f4a7d
sajad1993gh <sajad1993gh@gmail.com>
2019-06-02 19:26:22 -0400

    more

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,24 +12,27 @@ var crystals = [
 function start() {
     computerRandom = Math.floor(Math.random() * 102) + 19;
     $("#random").text(computerRandom);
-}
-
-start();
-$(document).ready(function() {
     $(".col-3").on("click", function() {
-        // console.log("hello");
-        crystalRandom = Math.floor(Math.random() * 12) + 1;
-        $(this).text(crystalRandom);
-        totalScore += crystalRandom;
-
-        $("#totalScore").text(totalScore);
-
-        if (totalScore > computerRandom) {
-            ++losses;
-            $("losses").text(losses);
-        } else if (totalScore === computerRandom) {
-            ++wins;
-            $("wins").text(wins);
+            crystalRandom = Math.floor(Math.random() * 12) + 1;
+            $(this).text(crystalRandom);
         }
+
+    }
+
+    start();
+    $(document).ready(function() {
+        $(".col-3").on("click", function() {
+            // console.log("hello");
+            totalScore += crystalRandom;
+
+            $("#totalScore").text(totalScore);
+
+            if (totalScore > computerRandom) {
+                ++losses;
+                $("losses").text(losses);
+            } else if (totalScore === computerRandom) {
+                ++wins;
+                $("wins").text(wins);
+            }
+        });
     });
-});
